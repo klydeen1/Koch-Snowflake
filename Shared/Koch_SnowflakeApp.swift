a44369c059f625f51a9fda78a2ea9c52625f9f60
Katelyn Lydeen <klydeen@hawk.iit.edu>
2022-02-14 14:38:19 -0600

    Initial Commit

---
+++ Shared/Koch_SnowflakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Koch_SnowflakeApp.swift
+//  Shared
+//
+//  Created by Katelyn Lydeen on 2/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Koch_SnowflakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
